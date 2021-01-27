6a3e82712e14c8a7b9709e2d02e6b212e8c92c82
OByrnes <liviabyrnes@gmail.com>
2021-01-27 16:50:48 -0500

    walkthrough

--- app.js
+++ app.js
@@ -10,17 +10,30 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+app.use((req, res, next)=>{
+  let {history} = req.session;
+  if (!history) {
+    history =[];
+    req.session.history = history
+  }
+  //console.log(`Protocol: ${req.protocol}, Host: ${req.get('host')}, url: ${req.originalUrl} `)
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+  // Add the URL to the beginning of the array.
+  history.unshift(url);
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home' ,history: req.session.history,});
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'About', history: req.session.history,});
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
+  res.render('contact', { title: 'Contact', history: req.session.history,});
 });
 
 const port = 8080;
